54cb0cd013a2fd9bc140ea17edf4f0dd75c8d441
JYPark09 <jyp10987@gmail.com>
2021-07-18 22:23:43 +0900

    feat(cpp): 랜덤봇 예제코드 업로드

--- cpp/RandomBot/main.cc
+++ cpp/RandomBot/main.cc
@@ -1,4 +1,57 @@
+#include <Arena/Agent.hpp>
+
+#include <random>
+
+class RandomBot final : public Arena::Agent
+{
+ public:
+    RandomBot() : Agent("RandomBot"), engine(std::random_device()())
+    {
+    }
+
+    void BoardSize(int boardSize) override
+    {
+        boardSize_ = boardSize;
+    }
+
+    void TimeSetting(int timesPerMove) override
+    {
+        timesPerMove_ = timesPerMove;
+    }
+
+    Arena::Point GenMove(Arena::StoneType color) override
+    {
+        std::uniform_int_distribution<int> dist(-1, boardSize_ - 1);
+
+        const int X = dist(engine);
+        const int Y = dist(engine);
+
+        if (X == -1 || Y == -1)
+            return Arena::Resign;
+
+        return Arena::Point{ X, Y };
+    }
+
+    void Play(Arena::StoneType oppColor, Arena::Point pt) override
+    {
+        (void)oppColor;
+        (void)pt;
+    }
+
+    void ClearBoard() override
+    {
+    }
+
+ private:
+    int boardSize_{ 0 };
+    int timesPerMove_{ 0 };
+
+    std::mt19937 engine;
+};
+
 int main()
 {
+    RandomBot bot;
 
+    bot.Run();
 }
