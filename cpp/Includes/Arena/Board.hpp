2bdb07ec9fb65dbc8aebc79b17cdb321f4f6803f
JYPark09 <jyp10987@gmail.com>
2021-07-20 00:28:03 +0900

    feat(cpp): 게임 코드 작성

---
+++ cpp/Includes/Arena/Board.hpp
@@ -0,0 +1,124 @@
+#ifndef ARENA_BOARD_HPP
+#define ARENA_BOARD_HPP
+
+#include <Arena/Types.hpp>
+
+#include <vector>
+
+namespace Arena
+{
+//!
+//! \brief 게임 보드판 클래스.
+//!
+class Board final
+{
+ public:
+    //! 생성자
+    //! \param boardSize 바둑판의 크기.
+    Board(int boardSize);
+
+    //! 기본 복사 생성자
+    Board(const Board&) = default;
+
+    //! 기본 이동 생성자
+    Board(Board&&) = default;
+
+    //! 기본 복사 할당자
+    Board& operator=(const Board&) = default;
+
+    //! 기본 이동 할당자
+    Board& operator=(Board&&) = default;
+
+    //! 바둑판의 크기를 반환합니다.
+    int GetBoardSize() const;
+
+    //! 해당 착수점이 바둑판 안에 있는지 체크하는 메소드.
+    //! \param pt 조사할 착수점 위치.
+    //! \return 바둑판 안에 있으면 true, 밖에 있으면 false를 반환.
+    bool IsOnBoard(const Point& pt) const;
+
+    //! 해당 착수점이 유효한지를 체크하는 메소드.
+    //! \param pt 조사할 착수점 위치.
+    //! \return 유효하면 true, 유효하지 않으면 false를 반환.
+    bool IsValidMove(const Point& pt) const;
+
+    //! 해당 착수점이 유효하지 않은 패인지 체크하는 메소드.
+    //! \param pt 조사할 착수점 위치.
+    //! \return 유효하지 않은 패이면 true, 그렇지 않으면 false를 반환.
+    bool IsKo(const Point& pt) const;
+
+    //! 해당 착수점이 자살수인지 체크하는 메소드.
+    //! \param pt 조사할 착수점 위치.
+    //! \return 자살수이면 true, 그렇지 않으면 false를 반환.
+    bool IsSuicide(const Point& pt) const;
+
+    //! 해당 위치의 돌의 색깔을 반환합니다.
+    //! \param pt 조사할 위치.
+    //! \return 해당 위치의 돌의 색깔.
+    StoneType At(const Point& pt) const;
+
+    //! 게임이 종료되었는지 여부를 반환합니다.
+    bool IsFinished() const;
+
+    //! 현재 둘 차례인 색을 반환합니다.
+    StoneType GetCurrentPlayer() const;
+
+    //! 승자를 반환합니다.
+    StoneType GetWinner() const;
+
+    //! 착수합니다.
+    //! \param pt 착수점 위치
+    //! \param color 착수할 돌의 색깔 (기본값으로 둘 경우 current_로 설정됨)
+    void Play(const Point& pt, StoneType color = StoneType::NONE);
+
+ public:
+    //!
+    //! \brief 이웃한 위치를 순회하는 이터레이터
+    //!
+    class NeighborIterator final
+    {
+     public:
+        NeighborIterator(const Board& bd, Point pt);
+
+        NeighborIterator& operator++();
+        NeighborIterator& operator--();
+
+        operator bool() const;
+        Point operator*() const;
+
+     private:
+        const Board& bd_;
+        Point pt_;
+        int offset_{ 0 };
+    };
+
+ private:
+    int IDX(const Point& pt) const;
+    int IDX(int x, int y) const;
+    Point PT(int idx) const;
+
+    int countNeighbors(int idx, StoneType color) const;
+    void addNeighbors(int idx, StoneType color);
+    void removeNeighbors(int idx, StoneType color);
+    void mergeGroup(int idx1, int idx2);
+    int removeGroup(int idx);
+
+    int countLine(int idx, StoneType color, bool includeEdge) const;
+    bool is1stOr2ndLine(int idx) const;
+
+ private:
+    int boardSize_;
+    Point ko_{ Resign };
+    StoneType current_{ StoneType::BLACK };
+    StoneType winner_{ StoneType::INVALID };
+    bool isFinished_{ false };
+
+    std::vector<StoneType> board_;
+    std::vector<int> parent_;
+    std::vector<int> next_;
+    std::vector<int> liberties_;
+    std::vector<std::uint16_t> neighbors_;
+};
+}  // namespace Arena
+
+#endif  // ARENA_BOARD_HPP
---
+++ cpp/Sources/Arena/Board.cc
@@ -0,0 +1,399 @@
+#include <Arena/Board.hpp>
+
+#include <array>
+
+namespace
+{
+constexpr std::uint16_t shift(Arena::StoneType type)
+{
+    return 4 * static_cast<std::uint16_t>(type);
+}
+}  // namespace
+
+namespace Arena
+{
+Board::Board(int boardSize) : boardSize_(boardSize)
+{
+    const int actualBoardSize = (boardSize + 2) * (boardSize + 2);
+
+    board_.resize(actualBoardSize);
+    parent_.resize(actualBoardSize);
+    next_.resize(actualBoardSize);
+    liberties_.resize(actualBoardSize);
+    neighbors_.resize(actualBoardSize);
+
+    std::fill(begin(board_), end(board_), StoneType::INVALID);
+    std::fill(begin(parent_), end(parent_), actualBoardSize);
+    std::fill(begin(next_), end(next_), actualBoardSize);
+    std::fill(begin(liberties_), end(liberties_), 0);
+    std::fill(begin(neighbors_), end(neighbors_), 0);
+
+    for (int y = 1; y <= boardSize; ++y)
+    {
+        for (int x = 1; x <= boardSize; ++x)
+        {
+            const int idx = IDX(x, y);
+
+            board_[idx] = StoneType::NONE;
+
+            neighbors_[idx] +=
+                ((1 + (x == 1 || x == boardSize)) << shift(StoneType::NONE));
+            neighbors_[idx] +=
+                ((1 + (y == 1 || y == boardSize)) << shift(StoneType::NONE));
+        }
+    }
+}
+
+int Board::GetBoardSize() const
+{
+    return boardSize_;
+}
+
+bool Board::IsOnBoard(const Point& pt) const
+{
+    return (1 <= pt.X && pt.X <= boardSize_) &&
+           (1 <= pt.Y && pt.Y <= boardSize_);
+}
+
+bool Board::IsValidMove(const Point& pt) const
+{
+    if (pt == Resign)
+        return true;
+
+    if (!IsOnBoard(pt) || (At(pt) != StoneType::NONE))
+        return false;
+
+    if (countLine(IDX(pt), current_, false) == 7)
+        return true;
+
+    if (IsSuicide(pt) || IsKo(pt))
+        return false;
+
+    if (current_ == StoneType::WHITE)
+        return true;
+
+    return countLine(IDX(pt), current_, true) < 8;
+}
+
+bool Board::IsKo(const Point& pt) const
+{
+    if (ko_ == Resign)
+        return false;
+
+    return ko_ == pt;
+}
+
+bool Board::IsSuicide(const Point& pt) const
+{
+    if (countNeighbors(IDX(pt), StoneType::NONE) > 0)
+        return false;
+
+    for (NeighborIterator it(*this, pt); it; ++it)
+    {
+        const int neighborIdx = IDX(*it);
+        const int libs = liberties_[parent_[neighborIdx]];
+
+        if (board_[neighborIdx] == current_ && libs > 1)
+            return false;
+
+        if (board_[neighborIdx] == Opponent(current_) && libs <= 1)
+            return false;
+    }
+
+    return true;
+}
+
+StoneType Board::At(const Point& pt) const
+{
+    return board_[IDX(pt)];
+}
+
+bool Board::IsFinished() const
+{
+    return isFinished_;
+}
+
+StoneType Board::GetCurrentPlayer() const
+{
+    return current_;
+}
+
+StoneType Board::GetWinner() const
+{
+    return winner_;
+}
+
+void Board::Play(const Point& pt, StoneType color)
+{
+    if (isFinished_)
+        return;
+
+    if (color != StoneType::NONE)
+        current_ = color;
+
+    ko_ = Resign;
+
+    if (pt == Resign || !IsValidMove(pt))
+    {
+        isFinished_ = true;
+        winner_ = Opponent(color);
+
+        return;
+    }
+
+    const int idx = IDX(pt);
+
+    board_[idx] = current_;
+    next_[idx] = idx;
+    parent_[idx] = idx;
+    liberties_[idx] = countNeighbors(idx, StoneType::NONE);
+    addNeighbors(idx, current_);
+
+    int capturedSize = 0;
+    int capturedIdx = 0;
+
+    for (NeighborIterator it(*this, pt); it; ++it)
+    {
+        const int neighborIdx = IDX(*it);
+
+        if (board_[neighborIdx] == current_)
+        {
+            const int pt1 = parent_[idx];
+            const int pt2 = parent_[neighborIdx];
+
+            mergeGroup(pt1, pt2);
+        }
+        else if (board_[neighborIdx] == Opponent(current_))
+        {
+            if (liberties_[parent_[neighborIdx]] <= 0)
+            {
+                const int captured = removeGroup(neighborIdx);
+
+                capturedSize += captured;
+                capturedIdx = neighborIdx;
+            }
+        }
+    }
+
+    if (capturedSize == 1 && (countNeighbors(capturedIdx, current_) == 4))
+    {
+        ko_ = PT(capturedIdx);
+    }
+
+    if (countLine(idx, current_, false) == 7)
+    {
+        isFinished_ = true;
+        winner_ = current_;
+    }
+
+    current_ = Opponent(current_);
+}
+
+int Board::IDX(const Point& pt) const
+{
+    return IDX(pt.X, pt.Y);
+}
+
+int Board::IDX(int x, int y) const
+{
+    return x + y * (boardSize_ + 2);
+}
+
+Point Board::PT(int idx) const
+{
+    const int x = idx % (boardSize_ + 2);
+    const int y = idx / (boardSize_ + 2);
+
+    return { x, y };
+}
+
+int Board::countNeighbors(int idx, StoneType color) const
+{
+    return (neighbors_[idx] >> shift(color)) & 0xF;
+}
+
+void Board::addNeighbors(int idx, StoneType color)
+{
+    std::array<int, 4> nearParents;
+    int count = 0;
+
+    for (NeighborIterator it(*this, PT(idx)); it; ++it)
+    {
+        const int neighborIdx = IDX(*it);
+
+        neighbors_[neighborIdx] += (1 << shift(color));
+        neighbors_[neighborIdx] -= (1 << shift(StoneType::NONE));
+
+        bool found = false;
+        for (int i = 0; i < count; ++i)
+        {
+            if (nearParents[i] == parent_[neighborIdx])
+            {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            --liberties_[parent_[neighborIdx]];
+            nearParents[count++] = parent_[neighborIdx];
+        }
+    }
+}
+
+void Board::removeNeighbors(int idx, StoneType color)
+{
+    std::array<int, 4> nearParents;
+    int count = 0;
+
+    for (NeighborIterator it(*this, PT(idx)); it; ++it)
+    {
+        const int neighborIdx = IDX(*it);
+
+        neighbors_[neighborIdx] -= (1 << shift(color));
+        neighbors_[neighborIdx] += (1 << shift(StoneType::NONE));
+
+        bool found = false;
+        for (int i = 0; i < count; ++i)
+        {
+            if (nearParents[i] == parent_[neighborIdx])
+            {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            --liberties_[parent_[neighborIdx]];
+            nearParents[count++] = parent_[neighborIdx];
+        }
+    }
+}
+
+void Board::mergeGroup(int idx1, int idx2)
+{
+    int idx = idx2;
+
+    do
+    {
+        for (NeighborIterator it(*this, PT(idx)); it; ++it)
+        {
+            bool found = false;
+            for (NeighborIterator it2(*this, *it); it2; ++it2)
+            {
+                if (parent_[IDX(*it2)] == idx1)
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                ++liberties_[idx1];
+            }
+        }
+
+        parent_[idx] = idx1;
+        idx = next_[idx];
+    } while (idx != idx2);
+
+    std::swap(next_[idx2], next_[idx1]);
+}
+
+int Board::removeGroup(int idx)
+{
+    int pos = idx;
+
+    int removed = 0;
+    const StoneType color = board_[idx];
+
+    do
+    {
+        board_[pos] = StoneType::NONE;
+        parent_[pos] = (boardSize_ + 2) * (boardSize_ + 2);
+
+        removeNeighbors(pos, color);
+
+        ++removed;
+        pos = next_[pos];
+    } while (pos != idx);
+
+    return removed;
+}
+
+int Board::countLine(int idx, StoneType color, bool includeEdge) const
+{
+    const int extendedSize = boardSize_ + 2;
+    const std::array<int, 4> dirs = { +1 - extendedSize, +1, +1 + extendedSize,
+                                      +extendedSize };
+
+    int maxCount = 0;
+
+    for (const int dir : dirs)
+    {
+        int count = 1;
+
+        for (int i = 1; board_[idx + i * dir] == color; ++i)
+        {
+            if (!includeEdge && is1stOr2ndLine(idx + i * dir))
+                break;
+
+            ++count;
+        }
+
+        for (int i = 1; board_[idx - i * dir] == color; ++i)
+        {
+            if (!includeEdge && is1stOr2ndLine(idx - i * dir))
+                break;
+
+            ++count;
+        }
+
+        maxCount = std::max(count, maxCount);
+    }
+
+    return maxCount;
+}
+
+bool Board::is1stOr2ndLine(int idx) const
+{
+    const Point pt = PT(idx);
+
+    return (pt.X <= 2 || pt.X >= boardSize_ - 1) ||
+           (pt.Y <= 2 || pt.Y >= boardSize_ - 1);
+}
+
+Board::NeighborIterator::NeighborIterator(const Board& bd, Point pt)
+    : bd_(bd), pt_(std::move(pt))
+{
+}
+
+Board::NeighborIterator& Board::NeighborIterator::operator++()
+{
+    ++offset_;
+    return *this;
+}
+
+Board::NeighborIterator& Board::NeighborIterator::operator--()
+{
+    --offset_;
+    return *this;
+}
+
+Board::NeighborIterator::operator bool() const
+{
+    return (0 <= offset_) && (offset_ < 4);
+}
+
+Point Board::NeighborIterator::operator*() const
+{
+    const int extendedSize = bd_.boardSize_ + 2;
+    const std::array<int, 4> dirs{ -1, +1, -extendedSize, +extendedSize };
+    const int idx = bd_.IDX(pt_) + dirs[offset_];
+
+    return bd_.PT(idx);
+}
+}  // namespace Arena
